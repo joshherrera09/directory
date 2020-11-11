bd8d0873f9aa02ad79ed9a5730c3484b6e1389b3
Josh Herrera <joshherrera09@gmail.com>
2020-11-11 12:58:45 -0800

    Container.js rough finished

--- directory/src/components/Container.js
+++ directory/src/components/Container.js
@@ -35,7 +35,44 @@ class Container extends Component {
                 order: "asc"
             })
         }
+    };
+
+    handleInputChange = event => {
+        const employees = this.state.employees;
+        const UserInput = event.target.value;
+        const filteredEmployees = employees.filter(employee => employee.name.first.toLowerCase().indexOf(UserInput.toLowerCase()) > -1)
+
+        this.setState({
+            filteredEmployees
+        })
+    };
+
+    employeeSearch = event => {
+        event.preventDefault();
+        if (!this.state.search) {
+            alert("Please enter a valid name")
+        }
+        const { employees, search } = this.state;
+        const filteredEmployees = employees.filter(employee => employee.name.first.toLowerCase().inludes(search.toLowerCase()));
+
+        this.setState({
+            filteredEmployees
+        });
     }
 
+    render() {
+        return (
+            <div>
+                <SearchBox
+                    employee={this.state.employees}
+                    handleSearch={this.handleSearch}
+                    handleInputChange={this.ahndleInputChange} />
+                <TableDate results={this.state.filteredEmployees}
+                    sortByName={this.sortByName} />
+            </div>
+        )
+    }
 
 }
+
+export default Container;
